5d7ffa531f2c7cc042beb25f89a0cbe2e8ddea92
Ronaldo Gomes <ronaldo.gomes@summit-bra.com>
2020-09-02 15:33:04 -0300

    Salvando o deck em um arquivo

--- deck.go
+++ deck.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"fmt" 
+	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -30,10 +31,14 @@ func (d deck) print() {
 	}
 }
 
-func deal(d deck, handSize int) (deck, deck){
+func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 }
 
-func (d deck) toString() string{
-	return strings.Join([]string(d), ",")	
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
